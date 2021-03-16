0e047eae9f36075fb66a8afd831fae6bd3ac465f
geraldelorm <geraldelorm14@gmail.com>
2021-03-16 18:31:11 +0000

    day 13 done

---
+++ Day-13-random-choice-picker/script.js
@@ -0,0 +1,67 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value);
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10);
+
+        randomSelect();
+    }
+});
+
+function createTags(input) {
+    const tags = input
+        .split(',')
+        .filter((tag) => tag.trim() != '')
+        .map((tag) => tag.trim());
+
+    tagsEl.innerHTML = '';
+
+    tags.forEach((tag) => {
+        const tagEl = document.createElement('span');
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl);
+    });
+}
+
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlight(randomTag);
+
+        setTimeout(() => {
+            endhighlight(randomTag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+            highlight(randomTag);
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlight(tag) {
+    tag.classList.add('highlight');
+}
+function endhighlight(tag) {
+    tag.classList.remove('highlight');
+}
