172bc1c71f2eaa344e7cef80321bf148c9e95e83
geraldelorm <geraldelorm14@gmail.com>
2021-03-19 15:03:03 +0000

    deletes

--- Day-4-hidden-search-widget/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.btn');
-const search = document.querySelector('.search');
-const input = document.querySelector('.input');
-
-btn.addEventListener('click', () => {
-    search.classList.toggle('active');
-    input.focus();
-});
